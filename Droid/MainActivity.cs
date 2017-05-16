499904ad36fc786fb52c09d132272b72abf305ca
Yury Nevalenny <nevalenny@lecta.ru>
2017-05-16 13:54:24 +0300

    add Mobile Center functionality

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,23 +1,27 @@
-using System;
-
 using Android.App;
-using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using Microsoft.Azure.Mobile.Distribute;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace SandboxForms.Droid
 {
     [Activity(Label = "SandboxForms.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://sandboxforms.azurewebsites.net");
+
         protected override void OnCreate(Bundle bundle)
         {
-            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+            CurrentPlatform.Init();
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+			MobileCenter.Start("0ed89836-e6fb-4fee-b7a3-218b1bb59108",
+				   typeof(Analytics), typeof(Crashes), typeof(Distribute), typeof(Microsoft.Azure.Mobile.Push.Push));
 
             base.OnCreate(bundle);
 
--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,7 +1,9 @@
+using Foundation;
 using Microsoft.Azure.Mobile;
 using Microsoft.Azure.Mobile.Analytics;
 using Microsoft.Azure.Mobile.Crashes;
-using Foundation;
+using Microsoft.Azure.Mobile.Distribute;
+using Microsoft.WindowsAzure.MobileServices;
 using UIKit;
 
 namespace SandboxForms.iOS
@@ -9,18 +11,20 @@ namespace SandboxForms.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        public static MobileServiceClient MobileService = new MobileServiceClient("https://sandboxforms.azurewebsites.net");
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
-            MobileCenter.Start("ac51556f-dfbb-4781-a43f-3ebf80ade230",
-                   typeof(Analytics), typeof(Crashes));
-            global::Xamarin.Forms.Forms.Init();
+            CurrentPlatform.Init();
+			MobileCenter.Start("31513391-7567-468b-85ac-7fb7b97d0e5b",
+				   typeof(Analytics), typeof(Crashes), typeof(Distribute), typeof(Microsoft.Azure.Mobile.Push.Push));
+
+			global::Xamarin.Forms.Forms.Init();
 
             // Code for starting up the Xamarin Test Cloud Agent
 #if ENABLE_TEST_CLOUD
             Xamarin.Calabash.Start();
 #endif
-
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
