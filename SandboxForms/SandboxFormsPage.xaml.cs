bb7d538e4fec830b259ad3f13236bea70286ce53
Yury Nevalenny <v-yuneva@microsoft.com>
2017-05-30 11:22:00 +0300

    add xlabs

--- SandboxForms/SandboxFormsPage.xaml.cs
+++ SandboxForms/SandboxFormsPage.xaml.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using Xamarin.Forms;
 
+[assembly: Xamarin.Forms.Xaml.XamlCompilation(Xamarin.Forms.Xaml.XamlCompilationOptions.Compile)]
 namespace SandboxForms
 {
-    public partial class SandboxFormsPage : ContentPage
+    public partial class SandboxFormsPage
     {
         public SandboxFormsPage()
         {
             InitializeComponent();
+
+            extendedLabel.Text = Humanizer.StringHumanizeExtensions.Humanize("Text test  stdff gdfgd Tet.Test");
+
         }
     }
 }
